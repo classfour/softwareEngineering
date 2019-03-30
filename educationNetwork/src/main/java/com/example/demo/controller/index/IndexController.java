9659cbc47104e6127daf11de63dda194deaa610b
klx <1148273143@qq.com>
2019-03-30 14:51:40 +0800

    完成登录界面，引入bootstrap4和jquery，加入阿里矢量图标

---
+++ educationNetwork/src/main/java/com/example/demo/controller/index/IndexController.java
@@ -0,0 +1,12 @@
+package com.example.demo.controller.index;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+    @RequestMapping("/index")
+    public String index() {
+        return "index/index";
+    }
+}
