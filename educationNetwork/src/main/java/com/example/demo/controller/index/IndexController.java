21c934d4c48773828bb54dd6b322c83733b9ec6c
klx <1148273143@qq.com>
2019-04-17 17:47:51 +0800

    完善本组目录结构，实现一个从数据库抓取课题信息并展示的小功能

--- educationNetwork/src/main/java/com/example/demo/controller/index/IndexController.java
+++ educationNetwork/src/main/java/com/example/demo/controller/index/IndexController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controller.index;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
