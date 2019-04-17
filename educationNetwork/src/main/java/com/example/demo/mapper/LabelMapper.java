cf5b30eda328f7923cbd0a2883a3f460f95ffbf9
klx <1148273143@qq.com>
2019-04-17 22:03:28 +0800

    添加label表数据库接口，暂未完善

---
+++ educationNetwork/src/main/java/com/example/demo/mapper/LabelMapper.java
@@ -0,0 +1,14 @@
+package com.example.demo.mapper;
+
+import com.example.demo.domain.Label;
+import org.apache.ibatis.annotations.Select;
+
+/**
+* @Description: 标签表接口 
+* @Author: klx 
+* @Date: 2019/4/17 
+*/
+public interface LabelMapper {
+    @Select("select * from label where name = #{name}")
+    public Label select(String name);
+}
