541eafac966947351a3745af8d736ddc5d3c7407
cordercorder <2205722269@qq.com>
2019-04-26 23:39:08 +0800

    enable echart to display data dynamically

--- educationNetwork/src/main/java/com/example/demo/controller/Score_management_system/Score_query.java
+++ educationNetwork/src/main/java/com/example/demo/controller/Score_management_system/Score_query.java
@@ -9,7 +9,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 public class Score_query {
@@ -21,6 +24,22 @@ public class Score_query {
         model.addAttribute("model",choose_courseService.Score_query("2016001"));
         return "index(groupFour)/score_query_test";
     }
+    @RequestMapping(value = "/score_query_json",method = RequestMethod.GET)
+    @ResponseBody
+    public Map<String,Object> sore_query_echart(){
+        List<ScoreEntity> lst=choose_courseService.Score_query("2016001");
+        List<Double> score=new ArrayList<Double>();
+        List<String> course_name=new ArrayList<String>();
+        Map<String,Object> mp=new HashMap<String,Object>();
+        for(int i=0;i<lst.size();i++){
+            score.add(lst.get(i).getTotal());
+            course_name.add(lst.get(i).getCoursename());
+        }
+        mp.put("course_name",course_name);
+        mp.put("score",score);
+        return mp;
+    }
+
     @ResponseBody
     @GetMapping("/test")
     public List<ScoreEntity> test(){
