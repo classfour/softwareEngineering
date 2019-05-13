8696ff12f2155cab68ce50dc366658018ac4e75a
zhushengtong <zhushengtong@163.com>
2019-05-13 08:16:48 +0800

    删除多余文件夹

---
+++ educationNetwork/src/main/resources/static/js/timetable.js
@@ -0,0 +1,19 @@
+window.onload = function() {
+    const initTable = () => {
+        var tabletext = '';
+
+        tabletext += '<thead><tr><th>节次</th><th>星期一</th><th>星期二</th><th>星期三</th><th>星期四</th><th>星期五</th><th>星期六</th><th>星期日</th></tr></thead>'
+        tabletext += '<tbody>';
+        for(var i=1; i<=13; i++){
+            tabletext += '<tr>';
+            tabletext += '<th>' + i + '</th>';
+            for(var j=1; j<=7; j++){
+                tabletext += '<th></th>'
+            }
+            tabletext += '</tr>';
+        }
+        tabletext += '</tbody>';
+        document.getElementById('timetable').innerHTML += tabletext;
+    }
+    initTable();
+}
